7bf2ca3dae4316fe8a28564326831db18fb857a3
“[Santosh <santoshjain19377@gmail.com>
2019-03-01 05:52:20 +0530

    commit for source-1

--- com.demo.springboot/src/main/java/com/demo/springboot/App.java
+++ com.demo.springboot/src/main/java/com/demo/springboot/App.java
@@ -20,11 +20,9 @@ public class App
         // a new text for testing
         // one more comment is added
         // comment - 2
-<<<<<<< HEAD
         //comment added after update from another source
-=======
 	// comment added from different source 
->>>>>>> 3860de93854dff38cc493352d9c5bd605a2d49a2
+    // demospring update
     }
 
 }
